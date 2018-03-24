36e151481e8ee70f69ee134e5b42d9b784172207
Nooreddein <nonosyousef@gmail.com>
2018-03-24 09:54:30 +0200

    second ass

--- algo-complexity/complexityAnalysis.js
+++ algo-complexity/complexityAnalysis.js
@@ -2,5 +2,10 @@
 // Be as clear and complete as necessary to demonstrate your mastery.
 // Write your explanation below as a comment
 /*
+  
+  (n called linear ) complexity when we have one for loop and statement to execute inside it.
 
+  (n*n called Quadratic) complexity when we have two for loop and statement to execute inside it.
+  
+  (Logarithmic) when we have for or while loop and inside them we search about specific element 
 */
